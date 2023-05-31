f0ddec5518d498253c3468f33dd693d00efea9b5
lualzugaray <98998748+lualzugaray@users.noreply.github.com>
2023-05-31 10:57:09 -0300

    Add files via upload

--- computienda/index.php
+++ computienda/index.php
@@ -6,6 +6,7 @@ include_once "header.php";
     <div class="inicio">
         <h1>Descubre <br>la inspiración <br>para construir una PC</h1>
         <div class="d-flex align-items-start flex-column" style="height: 200px;">
+            <button type="button" class="btn btn-danger">Ver más</button>
 
 
         </div>
