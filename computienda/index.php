220183a5242194ab6b574eef0c2332864cc6274c
lualzugaray <98998748+lualzugaray@users.noreply.github.com>
2023-05-30 21:53:36 -0300

    Add files via upload

---
+++ computienda/index.php
@@ -0,0 +1,12 @@
+<?php
+include_once "header.php";
+?>
+
+<div class="inicio">
+</div>
+
+
+
+<?php
+include_once "footer.php";
+?>
---
+++ computienda/nav.php
@@ -0,0 +1,49 @@
+<?php $pagina = ""; ?>
+
+<div class="contenedor">
+    <nav id="header" class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="">Computienda</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="miCuenta.php">Mi cuenta <i class="fa fa-user"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="miCarro.php">Mi carro <i class="fa fa-shopping-cart"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cerrar sesión <i class="fa fa-sign-out"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Categorías</span>
+            <div id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Promociones</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Laptops</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PC de escritorio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Monitores</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Placa de video</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
