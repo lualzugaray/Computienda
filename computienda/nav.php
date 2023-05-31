269c1cfe86a1fc1aad07df8cfba1fbafc876179a
lualzugaray <98998748+lualzugaray@users.noreply.github.com>
2023-05-31 00:06:23 -0300

    Add files via upload

--- computienda/nav.php
+++ computienda/nav.php
@@ -1,49 +1,53 @@
 <?php $pagina = ""; ?>
 
-<div class="contenedor">
-    <nav id="header" class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="">Computienda</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="miCuenta.php">Mi cuenta <i class="fa fa-user"></i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="miCarro.php">Mi carro <i class="fa fa-shopping-cart"></i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cerrar sesión <i class="fa fa-sign-out"></i></a>
-                    </li>
-                </ul>
+<div class="d-flex flex-column">
+    <div>
+        <nav id="header" class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="">Computienda</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav nav-fill w-100">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="miCuenta.php">Mi cuenta <i class="fa fa-user"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="miCarro.php">Mi carro <i class="fa fa-shopping-cart"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cerrar sesión <i class="fa fa-sign-out"></i></a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1">Categorías</span>
-            <div id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Promociones</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Laptops</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">PC de escritorio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Monitores</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Placa de video</a>
-                    </li>
-                </ul>
+        </nav>
+    </div>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">Categorías</span>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav nav-fill w-100">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Promociones</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Laptops</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">PC de escritorio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Monitores</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Placa de video</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 </div>
