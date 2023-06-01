e33e95d7c2e04eee27eda29b7d1063e49b6b68f2
lualzugaray <98998748+lualzugaray@users.noreply.github.com>
2023-05-31 23:14:12 -0300

    Add files via upload

--- computienda/index.php
+++ computienda/index.php
@@ -7,9 +7,32 @@ include_once "header.php";
         <h1>Descubre <br>la inspiración <br>para construir una PC</h1>
         <div class="d-flex align-items-start flex-column" style="height: 200px;">
             <button type="button" class="btn btn-danger">Ver más</button>
-
-
         </div>
+    </div>
+</section>
+
+<section id="presentacion">
+    <div class="contenedor">
+        <h2> <span>EQUIPO DE JUEGOS</span></h2>
+        <p>Empresa líder en tarjetas madre, tarjetas gráficas, laptops,hardware de juegos <br> y sistemas de alto rendimiento.
+            ¡Nos apasiona unirnos a los jugadores para <br> desafiar los límites sin miedo y luchar mientras nos elevamos a la gloria <br> máxima!</p>
+        <div class="container">
+            <div class="fila">
+                <div class="columna" >
+                    <img src="img/hyperx.jpg" width="150" height="150">
+                </div>
+                <div class="columna">
+                    <img src="img/logitech.jpg" width="150" height="150">
+                </div>
+                <div class="columna">
+                    <img src="img/razer.jpg" width="150" height="150">
+                </div>
+                <div class="columna">
+                    <img src="img/redragon.png" width="150" height="150">
+                </div>
+            </div>
+        </div>
+
     </div>
 </section>
 
