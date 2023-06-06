622a9b2a6b641cfeca221e8cdde198eb545f0feb
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-06-06 12:50:24 +0200

    Reorganizacion de archivos y carpetas. Creacion de file para ABM

---
+++ computienda/maquetado/header.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <title>Computienda</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nova+Square&family=Squada+One&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header class="my-header">
+
+        <?php require('nav.php') ?>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    </header>
---
+++ computienda/paginas/miCuenta.php
@@ -0,0 +1,13 @@
+<?php
+include_once "maquetado/header.php";
+?>
+
+<section id="miCuenta">
+    <div>
+        
+    </div>
+</section>
+
+<?php
+include_once "maquetado/footer.php";
+?>
