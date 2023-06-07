af3482e4ef4225430ca349bcdc98aa3a89d8138e
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-06-07 14:30:04 +0200

    Correcciones

--- computienda/miCarro.php
+++ computienda/miCarro.php
@@ -1,7 +1,8 @@
-<?php include_once "header.php"; ?>
+<?php session_start();
+ include_once "header.php"; ?>
 
 <?php
-session_start();
+
 if (!isset($_SESSION['carrito'])) {
     $_SESSION['carrito'] = array();
 }
