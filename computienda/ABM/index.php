e906b5f83133175c9f09d2ae567e8d3f2de7d163
lualzugaray <98998748+lualzugaray@users.noreply.github.com>
2023-06-06 22:14:16 -0300

    Add files via upload

--- computienda/ABM/index.php
+++ computienda/ABM/index.php
@@ -1,4 +1,4 @@
-<?php include_once "../header.php"; ?>
+<?php include_once "header.php"; ?>
 
 <?php
 
@@ -66,6 +66,6 @@ if (mysqli_connect('localhost', 'root', '', 'productos')) {
 
 <section class="footer" style="position: fixed;left: 0;bottom: 0;width: 100%;background-color: #000000;text-align: center;">
     <?php
-    include_once "../footer.php";
+    include_once "footer.php";
     ?>
 </section>
