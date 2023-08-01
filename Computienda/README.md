5f2b13df3a37c8add30e41206f0729f2fa60941a
Lucia Alzugaray <98998748+lualzugaray@users.noreply.github.com>
2023-08-01 16:49:12 -0300

    Delete Computienda directory

--- Computienda/README.md
+++
@@ -1,2 +0,0 @@
-# Computienda
-Trabajo integrador materia Produccion Web 
--- Computienda/computienda/controlador/logoutLogica.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-session_destroy();
-
-header("Location:../index.php");
-?>
-<!--La logica para cerrar sesión es simple: toma la sesión, y la termina con "session_destroy()". Luego nos devuelve al index-->
