ca1ce0ba5e0be9f247f7a998cc96286dbc9a2877
Lucia Alzugaray <98998748+lualzugaray@users.noreply.github.com>
2023-08-04 16:36:53 -0300

    Add files via upload

--- computienda/header.php
+++ computienda/header.php
@@ -2,7 +2,7 @@
 
 <head>
     <title>Computienda</title>
-    <link rel="stylesheet" href="http://localhost/Computienda/computienda/style/style.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/Computienda/computienda/style/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -21,8 +21,6 @@
     <header class="my-header">
 
         <?php require('nav.php') ?>
-
-
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </header>
